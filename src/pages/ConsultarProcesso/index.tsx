8dc3e3451302014b68dc8dddf07c0c78950d554b
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-09-26 10:43:36 -0300

    criando lances no processo

--- src/pages/ConsultarProcesso/index.tsx
+++ src/pages/ConsultarProcesso/index.tsx
@@ -4,9 +4,10 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import api from "../../services/api";
 
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 interface IProcesso {
+    lance_atual_id: string;
     numero: string;
     reclamante: string;
     reclamado: string;
@@ -16,18 +17,19 @@ interface IProcesso {
     valor_incremento: number;
 }
 
+interface ILance {
+    valor: number
+}
+
 function ConsultarProcesso() {
     const [processo, setProcesso] = useState<IProcesso>({} as IProcesso);
+    const [maiorLance, setMaiorLance] = useState<ILance>({} as ILance);
 
     const [numero_processo, setNumeroProcesso] = useState("");
-    const [reclamante, setReclamante] = useState("");
-    const [reclamado, setReclamado] = useState("");
-    const [tipo, setTipo] = useState("imovel");
-    const [valor, setValor] = useState(0);
-    const [valor_inicial, setValorInicial] = useState(0);
-    const [valor_incremento, setValorIncremento] = useState(0);
 
-    async function handleSubmit() {
+    const [valor_lance, setValorLance] = useState(0);
+
+    async function handleConsultarProcesso() {
         try
         {
             const response = await api.get("/processos/consultar", {
@@ -36,9 +38,9 @@ function ConsultarProcesso() {
                 }
             })
 
-            console.log(response.data);
-
             setProcesso(response.data);
+
+            await getMaiorLance(response.data.lance_atual_id);
 
             toast.success("Processo consultado com sucesso!");
         }
@@ -50,17 +52,77 @@ function ConsultarProcesso() {
         }
     }
 
+    async function handleCriarLance() {
+        try
+        {
+            const response = await api.post("/processos/lance", {
+                numero_processo,
+                valor: valor_lance
+            })
+
+            console.log(response.data);
+
+            toast.success("Lance criado com sucesso!");
+        }
+        catch(err) {
+            console.log(err.response.data);
+            const responseData = err.response.data;
+
+            toast.error(`Ocorreu um erro ao criar o lance. ${responseData.message}`)
+        }
+    }
+
+    async function getMaiorLance(lance_id: string) {
+        if (!lance_id) {
+            return;
+        }
+
+        try
+        {
+            const response = await api.get("/processos/lance", {
+                params: {
+                    lance_id
+                }
+            })
+
+            console.log(response.data);
+
+            setMaiorLance(response.data);
+        }
+        catch(err) {
+            console.log(err.response.data);
+            const responseData = err.response.data;
+
+            toast.error(`Ocorreu um erro ao consultar o lance. ${responseData.message}`)
+        }
+    }
+
     return (
         <Container>
-            <h1>Consultar processo</h1>
-            <Input onChange={(event) => setNumeroProcesso(event.target.value)} placeholder="Numero do processo"/>
-            <Input disabled value={processo.reclamante} placeholder="Reclamante"/>
-            <Input disabled value={processo.reclamado} placeholder="Reclamado"/>
-            <Input disabled value={processo.tipo} placeholder="Reclamado"/>
-            <Input disabled value={processo.valor} placeholder="Valor"/>
-            <Input disabled value={processo.valor_inicial} placeholder="Valor inicial"/>
-            <Input disabled value={processo.valor_incremento} placeholder="Valor incremento"/>
-            <Button onClick={handleSubmit}>Consultar</Button>
+            <Content>
+                <h1>Consultar processo</h1>
+                <Input onChange={(event) => setNumeroProcesso(event.target.value)} placeholder="Numero do processo"/>
+                <Input disabled value={processo.reclamante} placeholder="Reclamante"/>
+                <Input disabled value={processo.reclamado} placeholder="Reclamado"/>
+                <Input disabled value={processo.tipo} placeholder="Reclamado"/>
+                <Input disabled value={processo.valor} placeholder="Valor"/>
+                <Input disabled value={processo.valor_inicial} placeholder="Valor inicial"/>
+                <Input disabled value={processo.valor_incremento} placeholder="Valor incremento"/>
+                <Button onClick={handleConsultarProcesso}>Consultar</Button>
+            </Content>
+
+            {
+                processo.numero && 
+                <Content>
+                    <h1>Adicionar lance</h1>
+                    {
+                        maiorLance && 
+                        <Input disabled value={maiorLance.valor} placeholder="Lance atual"/>
+                    }
+                    <Input onChange={(event) => setValorLance(Number(event.target.value))} placeholder="Valor do lance"/>
+                    <Button onClick={handleCriarLance}>Adicionar</Button>
+                </Content>
+            }
         </Container>
     )
 }
--- src/pages/ConsultarProcesso/styles.ts
+++ src/pages/ConsultarProcesso/styles.ts
@@ -2,34 +2,26 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    align-items: center;
+    /* align-items: center; */
     height: 100vh;
-    flex-direction: column;
-    gap: 8px;
+    flex-direction: row;
 
-    input, div {
-        width: 300px;
-    }
 `;
 
-export const Content = styled.main`
+export const Content = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
     flex-direction: column;
-
+    gap: 8px;
+    
     margin-top: 12vh;
-`;
-
-export const Form = styled.div`
-    flex-direction: column;
-    margin-top: 20px;
-
-    input {
-        margin-top: 8px;
+    
+    input, div {
+        width: 300px;
     }
 
     button {
-        margin-top: 18px;
+        margin-top: 12px;
     }
 `;
